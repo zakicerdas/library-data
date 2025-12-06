560f202ffb9b60a1ccf94b0ce91d40a69e66fa43
zakicerdas <zakitoriq1@gmail.com>
2025-12-06 08:45:45 +0700

    added error handler

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -5,9 +5,9 @@ import { NODE_ENV } from '../utils/env';
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   console.error('ERROR:', err.message);
 
-  const statusCode = err.message.includes('tidak ditemukan') ? 404 : 400;
+  const statusCode = err.message.includes('not founded') ? 404 : 400;
 
-  errorResponse(res, err.message || 'Terjadi kesalahan server', statusCode, 
+  errorResponse(res, err.message || 'error founded', statusCode, 
     NODE_ENV === 'development' ? { stack: err.stack } : null
   );
 };
